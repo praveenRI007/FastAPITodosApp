8b8454498ea180c354f00737e27a917f3b9e6a04
Praveen <praveenriemplfizh@gmail.com>
2023-10-01 16:37:18 +0530

    startup and shutdown for DB , cors middleware - added

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import text
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 SQLALCHEMY_PGRES_DATABASE_URL = 'postgresql://mrayhbnk:7RqHvrjBbM6LL4KO0Zq6EGdH7mdcsyUE@berry.db.elephantsql.com/mrayhbnk'
@@ -19,3 +20,23 @@ engine = create_engine(SQLALCHEMY_PGRES_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+async def check_db_connected():
+    try:
+        database = SessionLocal()
+        database.execute(text("SELECT 1"))
+        print("Database is connected (^_^)")
+    except Exception as e:
+        print("Looks like there is some problem in connection,see below traceback")
+        raise e
+
+
+async def check_db_disconnected():
+    try:
+        database = SessionLocal()
+        database.close()
+        print("Database is Disconnected (-_-) zZZ")
+    except Exception as e:
+        raise e
+
--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from fastapi import FastAPI, Depends
+from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import RedirectResponse
 
 import models
-from database import engine
+from database import engine, check_db_connected, check_db_disconnected
 from routers import auth, todos , users
 from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -16,6 +30,15 @@ app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(users.router)
 
+
+@app.on_event("startup")
+async def app_startup():
+    await check_db_connected()
+
+
+@app.on_event("shutdown")
+async def app_shutdown():
+    await check_db_disconnected()
 
 @app.get("/")
 async def root():
