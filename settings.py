83b029804a892582a3505b01d2ecfd15efee72ca
Praveen <praveenriemplfizh@gmail.com>
2024-04-03 13:08:33 +0530

    disabling mongodb

--- settings.py
+++ settings.py
@@ -6,4 +6,4 @@ class Settings(BaseSettings):
     SQLALCHEMY_PGRES_DATABASE_URL: str = "postgresql://mrayhbnk:7RqHvrjBbM6LL4KO0Zq6EGdH7mdcsyUE@berry.db.elephantsql.com/mrayhbnk"
     SECRET_KEY = "KlgH6AzYDeZeGwD288to79I3vTHT8wp7"
     ALGORITHM = "HS256"
-    isMongoTrue: bool = True
+    isMongoTrue: bool = False
